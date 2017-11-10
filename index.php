ac6aff2fbb4f901ae7ae1fcafb906503bc58aa95
19051976 <the@amok.space>
2017-11-10 15:57:03 +0200

    I fixed

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ echo('My name is 19051976. What about ya?');
 <<<<<<< HEAD
 echo('I am about to work during my holy weekend a little.');
 
+echo doSomething();
+
 function doSomething()
 {
 	return 'nothing ^)';
 }
-=======
->>>>>>> parent of 8ee5498... THe first line has been done.
