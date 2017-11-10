6c1765c946d7878731da9bc751b0eea331984cd4
19051976 <the@amok.space>
2017-11-10 15:50:54 +0200

    Weird things are all around

--- index.php
+++ index.php
@@ -3,3 +3,8 @@
 echo('Hello World!');
 echo('My name is 19051976. What about ya?');
 echo('I am about to work during my holy weekend a little.');
+
+function doSomething()
+{
+	return 'nothing ^)';
+}
