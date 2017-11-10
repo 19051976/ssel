414ae51073b2f42d0c58a31ecc43aec6f4095d48
19051976 <the@amok.space>
2017-11-10 15:55:38 +0200

    Revert "THe first line has been done."
    This reverts commit 8ee54980eca917dbd5c8eeeedcdac901620c7640.

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 
 echo('Hello World!');
 echo('My name is 19051976. What about ya?');
+<<<<<<< HEAD
 echo('I am about to work during my holy weekend a little.');
 
 function doSomething()
 {
 	return 'nothing ^)';
 }
+=======
+>>>>>>> parent of 8ee5498... THe first line has been done.
